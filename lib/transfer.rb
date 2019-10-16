b9f71f218c28b32518320edb47c86e27203514f8
generous-vector-9581 <ojappah@gmail.com>
2019-10-16 19:51:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,3 +29,13 @@ class Transfer
       @status = "complete"
     end
   end
+  
+    def reverse_transfer
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
+  end
+
+end
